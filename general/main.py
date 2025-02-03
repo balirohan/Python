b10ab33d550be05c049f0e4ec6824c54965a8adb
Rohan Bali <balirohan301@gmail.com>
2025-02-03 10:32:48 +0530

    added general folder with main.py and vars.py

---
+++ general/main.py
@@ -0,0 +1,3 @@
+# This is my first Python program
+print("I like Spaghetti!")
+print("It's really good!")
---
+++ general/vars.py
@@ -0,0 +1,46 @@
+# Variable = A container for a value (string, integer, float, boolean)
+#            A variable behaves as if it was the value it contains
+
+# Strings
+first_name = "Rohan"
+food = "Spaghetti"
+email = "balirohan302@gmail.com"
+
+# print(f"Hello {first_name}")
+# print(f"You like {food}")
+# print(f"Your email is: {email}")
+
+# Integers
+age = 22
+quantity = 3
+num_of_students = 30
+
+# print(f"You are {age} years old")
+# print(f"You are buying {quantity} items")
+# print(f"Your class has {num_of_students} students")
+
+# Float
+price = 10.99
+gpa = 7.75
+distance = 5.5
+
+# print(f"The price is ${price}")
+# print(f"Your gpa is {gpa}")
+# print(f"You ran {distance}km")
+
+# Boolean
+is_student = True
+for_sale = True
+is_online = True
+
+# if is_online:
+#     print("You are online")
+# else:
+#     print("You are offline")
+
+# if for_sale:
+#     print("That item is for sale")
+# else:
+#     print("That item is not available")
+
+# print(f"Are you a student?: {is_student}")
