3ea249ab4b3b606649c4e48420e7bb31b38ec800
Rohan Bali <balirohan301@gmail.com>
2025-02-03 11:38:30 +0530

    practiced math in /general/math.py

---
+++ general/math.py
@@ -0,0 +1,38 @@
+import math
+
+friends = 10
+
+# friends = friends + 1
+# friends += 1
+# friends = friends - 2
+# friends -= 2
+# friends = friends * 3
+# friends *= 3
+# friends = friends / 2
+# friends /= 2
+# friends = friends ** 2
+# friends **=2
+# remainder = friends % 3
+
+# print(f"{friends} friends")
+# print(f"{remainder} friend left :(")
+
+x = 3.14
+y = 4
+z = 5
+
+# result = round(x)
+# result = abs(y)
+# result = pow(y, 3)
+# result = max(x, y, z)
+# result = min(x, y, z)
+
+# print(result)
+
+# print(math.pi)
+# print(math.e)
+# result = math.sqrt(9) --> float
+# result = math.ceil(9.1) --> int
+# result = math.floor(9.9) --> int
+
+# print(result)
