832ad42ef2f7bb3cd0f7e5f931c90a3e2885709b
Rohan Bali <balirohan301@gmail.com>
2025-02-03 12:37:40 +0530

    learnt about if statements in main.py

--- general/main.py
+++ general/main.py
@@ -2,14 +2,55 @@
 # print("I like Spaghetti!")
 # print("It's really good!")
 
+#########################################################
+
 # input() = A function that prompts the user to enter data
 #           Returns the entered data as a string
 
-name = input("What is your name?: ")
-age = int(input("How old are you?: "))
+# name = input("What is your name?: ")
+# age = int(input("How old are you?: "))
 
-age += 1
+# age += 1
 
-print(f"Hello {name}!")
-print("HAPPY BIRTHDAY!")
-print(f"You are {age} years old!")
+# print(f"Hello {name}!")
+# print("HAPPY BIRTHDAY!")
+# print(f"You are {age} years old!")
+
+#########################################################
+
+# if = Do some code only IF some condition is True
+#      else do something else
+
+# age = int(input("Enter your age: "))
+
+# if age >= 100:
+#     print("You are too old to sign up.")
+# elif age >= 18:
+#     print("You are now signed up!")
+# elif age < 0:
+#     print("You haven't been born yet.")
+# else:
+#     print("You must be 18+ to sign up.")
+
+# response  = input("Would you like food (Y/N): ").lower()
+
+# if response == 'y':
+#     print("Have some food!")
+# else:
+#     print("No food for you")
+
+# name = input("Enter your name: ").lower()
+
+# if name == "":
+#     print("YOU DID NOT TYPE IN YOUR NAME!")
+# else:
+#     print(f"Hello {name}!")
+
+# for_sale = True
+
+# if for_sale:
+#     print("This item is for sale!")
+# else:
+#     print("This item is not for sale.")
+
+#########################################################
