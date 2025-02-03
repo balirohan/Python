cbb74259e768314b28b1ea9fea9b9cf2c33d0a6d
Rohan Bali <balirohan301@gmail.com>
2025-02-03 10:55:07 +0530

    added typecast.py and learnt to take user input() in main.py

--- general/main.py
+++ general/main.py
@@ -1,3 +1,15 @@
 # This is my first Python program
-print("I like Spaghetti!")
-print("It's really good!")
+# print("I like Spaghetti!")
+# print("It's really good!")
+
+# input() = A function that prompts the user to enter data
+#           Returns the entered data as a string
+
+name = input("What is your name?: ")
+age = int(input("How old are you?: "))
+
+age += 1
+
+print(f"Hello {name}!")
+print("HAPPY BIRTHDAY!")
+print(f"You are {age} years old!")
---
+++ general/typecast.py
@@ -0,0 +1,14 @@
+# Typecasting = the process of converting a variable from one data type to another
+#               str(), int(), float(), bool()
+
+name = ""
+age = 22
+gpa = 7.75
+is_student = True
+
+gpa = int(gpa)
+age = float(age)
+age = str(age)
+name = bool(name)
+
+print(name)
