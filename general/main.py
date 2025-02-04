0d30a98344be556f345abf3cc1dff16d813a0366
Rohan Bali <balirohan301@gmail.com>
2025-02-04 16:42:05 +0530

    while loop in /general/main.py and added /mini-projects/compound-interest.py

--- general/main.py
+++ general/main.py
@@ -149,3 +149,85 @@
 # print(result)
 
 # print(help(str)) --> to list out all the methods of this data type
+
+#########################################################
+
+# String Indexing
+
+# indexing = accessing elements of a sequence using [] (indexing operator)
+#            [start : end : step]
+
+# credit_number = "1234-5678-9012-3456"
+
+# reversed_card = credit_number[::-1]
+# print(reversed_card)
+
+# last_digits = credit_number[-4:]
+# print(f"XXXX-XXXX-XXXX-{last_digits}")
+
+# print(credit_number[2])
+# print(credit_number[:4])
+# print(credit_number[5:9])
+# print(credit_number[-4:])
+# print(credit_number[::2])
+
+#########################################################
+
+# format specifiers = {value:flags} format a value based on what flags are inserted
+
+# price1 = 3000.14159
+# price2 = -9870.65
+# price3 = 1200.34
+
+# print(f"Price 1 is ${price1:+,.2f}")
+# print(f"Price 2 is ${price2:+,.2f}")
+# print(f"Price 3 is ${price3:+,.2f}")
+
+#########################################################
+
+# while loop = execute some code WHILE some condition remains True
+
+# name = input("Enter your name: ")
+
+# while name == "":
+#     print("You did not enter your name")
+#     name = input("Enter your name: ")
+
+# print(f"Hello {name}")
+
+
+
+
+# age = int(input("Enter your age: "))
+
+# while age <= 0:
+#     print("Age can't be negative or zero.")
+#     age = int(input("Enter your age: "))
+
+# print(f"You are {age} years old!")
+
+
+
+
+# food = input("Enter a food you like (q to quit): ")
+
+# while food.lower() != "q":
+#     print(f"You like {food}.")
+#     food = input("Enter another food you like (q to quit): ")
+
+# print('bye')
+
+
+
+
+# num = int(input("Enter a number between 1-10: "))
+
+# while num < 1 or num > 10:
+#     print(f"{num} is not valid!")
+#     num = int(input("Enter a number between 1-10: "))
+
+# print(f"Your number is {num}")
+
+#########################################################
+
+# for loops = execute a block of code 
