ae1bae36e0e78c1937cd78ea0ff7039a87aa4481
Rohan Bali <balirohan301@gmail.com>
2025-02-11 16:25:50 +0530

    function.py and return.py in /general/,,

---
+++ general/function.py
@@ -0,0 +1,28 @@
+# function = A block of reusable code place () after the function name to invoke it
+
+# default arguments
+# def happy_birthday(name, age, times):
+#     for _ in range(times):
+#         print(f"Happy Birthday to {name}!")
+#         print(f"You are {age} years old!")
+#         print("Happy Birthday to you!")
+#         print()
+
+# happy_birthday("Rohan", 22, 3)
+
+
+# positional arguments
+# def happy_birthday(times, name = "John", age = 21):
+#     for _ in range(times):
+#         print(f"Happy Birthday to {name}!")
+#         print(f"You are {age} years old!")
+#         print("Happy Birthday to you!")
+#         print()
+
+# happy_birthday(3, age = 22)
+
+# def display_invoice(username, amount, due_date):
+#     print(f"Hello {username}")
+#     print(f"Your bill of ${amount:.2f} is due on {due_date}")
+
+# display_invoice("BroCode", 42.50, "15/02/2025")
---
+++ general/return.py
@@ -0,0 +1,27 @@
+# return = statement used to end a function and send a result back to the caller of the function
+
+
+# def add(x, y):
+#     return x + y
+
+# def subtract(x, y):
+#     return x - y
+
+# def multiply(x, y):
+#     return x * y
+
+# def divide(x, y):
+#     return x / y
+
+# print(add(1, 2))
+# print(subtract(1, 2))
+# print(multiply(1, 2))
+# print(divide(1, 2))
+
+
+# def create_name(first, last):
+#     first = first.capitalize()
+#     last = last.capitalize()
+#     return first+" "+last
+
+# print(create_name("rohan", "bali"))
