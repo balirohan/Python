ee2da7f6e8db421165d8eb9e335be52bec7c10df
Rohan Bali <balirohan301@gmail.com>
2025-02-12 13:01:22 +0530

    modules in general/module.py and general/example.py

---
+++ general/module.py
@@ -0,0 +1,32 @@
+# module = a file containing code you want to include in your program
+#          use 'import' to include a module (built-in or your own)
+#          useful to break up a large program into reusable separate files
+
+# print(help("modules"))
+
+# import math
+# import math as m
+# from math import pi
+# from math import e
+
+# print(math.sqrt(9.64))
+# print(m.pi)
+# print(pi)
+
+# a, b, c, d, e = 1, 2, 3, 4, 5
+
+# print(e ** a)
+# print(e ** b)
+# print(e ** c)
+# print(e ** d)
+# print(e ** e)
+
+# import example
+
+# result = example.pi
+# result = example.square(3)
+# result = example.cube(3)
+# result = example.circumference(3)
+# result = example.area(3)
+
+print(result)
