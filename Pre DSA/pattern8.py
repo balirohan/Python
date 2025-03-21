0a896f8c6df6893d98ad89e2798666cf29cc645f
Rohan Bali <balirohan301@gmail.com>
2025-03-21 16:52:28 +0530

    Star pattern in pattern 8

---
+++ Pre DSA/pattern8.py
@@ -0,0 +1,18 @@
+#  PATTERN 7
+
+#    *********
+#     *******
+#      *****
+#       ***
+#        *
+
+t = int(input())
+
+for i in range(t):
+    n = int(input())
+    for __ in range(n):
+        for space in range(__):
+            print(" ",end="")
+        for star in range(2*(n-__)-1):
+            print("*", end="")
+        print()
