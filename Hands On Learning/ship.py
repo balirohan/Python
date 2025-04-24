b52023889a67b59184d3e1a03412850621c4bc04
Rohan Bali <balirohan301@gmail.com>
2025-04-24 16:08:17 +0530

    Alien Invasion project using pygame in /Hands On Learning

---
+++ Hands On Learning/ship.py
@@ -0,0 +1,50 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Ship(Sprite):
+    """A class to manage the ship."""
+
+    def __init__(self, game):
+        """Initialize the ship and set it's starting position."""
+        super().__init__()
+        self.screen = game.screen
+        self.settings = game.settings
+        self.screen_rect = game.screen.get_rect()
+
+        # Load the ship image and it's rect.
+        self.original_image = pygame.image.load('Hands On Learning\images\ship.bmp')
+
+        self.new_width = 75
+        self.new_height = 75
+
+        self.image = pygame.transform.scale(self.original_image, (self.new_width, self.new_height))
+        self.rect = self.image.get_rect()
+
+        # Start each new ship at the bottom center of the screen.
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        # Store a decimal value for the ship's horizontal position
+        self.x = float(self.rect.x)
+
+        self.moving_right = False
+        self.moving_left = False
+
+    def blitme(self):
+        """Draw the ship at it's current location."""
+        self.screen.blit(self.image, self.rect)
+
+    def update(self):
+        """Update the ship's position based on the movement flag."""
+        # Update the ship's x value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x
+        self.rect.x = self.x
+
+    def center_ship(self):
+        """Center the ship on the screen."""
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
